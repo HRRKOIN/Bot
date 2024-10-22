94c5466a4195122cbb9c995729e957f2e8cd9e08
Jaimin Patel <1994.jaimin@gmail.com>
2024-10-23 00:11:01 +0530

    Added Qr Code in telegram bot

--- bot.js
+++ bot.js
@@ -1,9 +1,20 @@
 const TelegramBot = require('node-telegram-bot-api');
+const QRCode = require('qrcode');
+
 require('dotenv').config();
 const bot = new TelegramBot(process.env.TOKEN, { polling: true });
 
+async function generateQRCode(url) {
+  return new Promise((resolve, reject) => {
+    QRCode.toBuffer(url, function (err, buffer) {
+      if (err) reject(err);
+      resolve(buffer);
+    });
+  });
+}
+
 // Handle /start 
-bot.onText(/\/start/, (msg) => {
+bot.onText(/\/start/, async(msg) => {
   const chatId = msg.chat.id;
   let reffralId = msg.text.split(' ');
   let url = ''
@@ -12,8 +23,17 @@ bot.onText(/\/start/, (msg) => {
   } else {
     url = `${process.env.URL}`
   }
-  if (msg.web_app) {
-    bot.sendMessage(chatId, "Sorry, this bot is restricted to mobile usage.");
+  const isDesktop = msg.web_app || true;  // Replace this condition as per your needs
+
+  if (isDesktop) {
+    // Generate a QR code linking to your bot's URL (for mobile scanning)
+    const mobileBotURL = `https://t.me/JobMiningbot`;
+    const qrCodeBuffer = await generateQRCode(mobileBotURL);
+
+    // Send message and QR code image
+    const messageText = "Please switch to your mobile and scan the QR code to play!";
+    bot.sendMessage(chatId, messageText);
+    bot.sendPhoto(chatId, qrCodeBuffer);
   } else {
     bot.sendMessage(chatId, `Hello, ${msg.from.first_name}!!\n\nWelcome to LinkingJobs!!.\n\nLinkingJobs is building the Web3 Future of Work'—a world of shared opportunities, collective ownership, and earning potential for everyone.\n\nA unique profit-sharing project where you can earn not only tokens but also real cash.\n\nJoin the community and be part of the largest airdrop in history!`, {
       reply_markup: {
